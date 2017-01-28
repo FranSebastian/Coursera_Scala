c96b637d73a82b6d4dee257d03253f72e746b20d
Fran Sebastian <Fran Sebastian@FranSebastian>
2017-01-28 20:52:26 +0100

    first commit

---
+++ hello-project/.worksheet/src/greeter.generalWorkshit.scala
@@ -0,0 +1,14 @@
+package greeter
+
+object generalWorkshit {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(85); 
+  println("Welcome to the Scala worksheet");$skip(23); 
+  
+  val testint = 2+2;System.out.println("""testint  : Int = """ + $show(testint ));$skip(15); 
+  
+  val x = 1;System.out.println("""x  : Int = """ + $show(x ));$skip(31); 
+  def increase(i: Int) = i + 1;System.out.println("""increase: (i: Int)Int""");$skip(14); val res$0 = 
+  increase(x);System.out.println("""res0: Int = """ + $show(res$0));$skip(25); 
+  
+  val y = List(5,2,1);System.out.println("""y  : List[Int] = """ + $show(y ))}
+  
+}
---
+++ hello-project/src/greeter/generalWorkshit.sc
@@ -0,0 +1,14 @@
+package greeter
+
+object generalWorkshit {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+  val testint = 2+2                               //> testint  : Int = 4
+  
+  val x = 1                                       //> x  : Int = 1
+  def increase(i: Int) = i + 1                    //> increase: (i: Int)Int
+  increase(x)                                     //> res0: Int = 2
+  
+  val y = List(5,2,1)                             //> y  : List[Int] = List(5, 2, 1)
+  
+}
